8e46eb25e1034054444d597db7cf070b94e65582
Hector Orozco <hect05661@gmail.com>
2022-09-06 10:03:48 -0700

    Initialization of our component

---
+++ gym-trainer/src/components/DayView/DayView.tsx
@@ -0,0 +1,20 @@
+// Testing to see if github pushes work!
+
+
+
+
+export default function DayView() {
+
+    // Return a modal (created in bootstrap)
+    // display information about the day's workout regime
+    // Have the week view have an onclick() that will display this component
+    return <>
+
+    
+    
+    
+    
+    
+    </>
+
+}
